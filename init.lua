95f8eedacb7b93362fc124ecfb629aea2d80a07b
hzy-hits <1106796240@qq.com>
2024-04-19 22:24:07 -0400

    almost forgot the init

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.cmd([[colorscheme solarized-osaka]])
+vim.api.nvim_set_hl(0, "VertSplit", { fg = "#FFFFFF", bg = "#303030" })
 local powershell_options = {
   shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
   shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
@@ -11,6 +13,26 @@ local powershell_options = {
 
 for option, value in pairs(powershell_options) do
   vim.opt[option] = value
+end
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_transparency = 0.87
+  vim.g.neovide_theme = "dark"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_refresh_rate = 60
+  vim.keymap.set("n", "<C-s>", ":w<CR>", { silent = true })
+  -- 在可视模式和普通模式下从系统剪贴板复制和粘贴
+  vim.keymap.set("v", "<C-c>", '"+y', { silent = true }) -- 复制
+  vim.keymap.set("n", "<C-v>", '"+p', { silent = true }) -- 普通模式粘贴
+  vim.keymap.set("v", "<C-v>", '"+p', { silent = true }) -- 可视模式粘贴
+  vim.keymap.set("v", "<C-v>", '"+p', { silent = true })
+  -- 在命令行模式下粘贴
+  vim.keymap.set("c", "<C-v>", "<C-R>+", { silent = true })
+  -- 在插入模式下粘贴
+  vim.keymap.set("i", "<C-v>", "<C-R>+", { silent = true })
+  -- 如果使用终端模式
+  vim.keymap.set("t", "<C-v>", "<C-R>+", { silent = true })
 end
 
 --if vim.g.neovide then
@@ -26,7 +48,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
     if vim.g.neovide then
       vim.defer_fn(function()
         vim.fn.system("im-select.exe 1033")
-      end, 0) -- 延迟1000毫秒执行
+      end, 0)
     end
   end,
 })
